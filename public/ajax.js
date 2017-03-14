6daed9ba490697051df684231d35f47cf9d1b78c
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-03-13 21:32:28 -0700

    Made a post request with jQuery

--- public/ajax.js
+++ public/ajax.js
@@ -1,3 +1,12 @@
-$.get('/todos', function(data){
-    debugger
+$.get('/todos', function(data) {
+    
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    $.post('/todos', formData, function(data){
+        console.log(data);
+    });
+
 });
