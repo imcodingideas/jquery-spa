e509cf1f104dd7dacc37248f6f8f1a760bb4cfea
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-03-13 23:11:27 -0700

    submitted a DELETE request to API with AJAX

--- app.js
+++ app.js
@@ -64,15 +64,11 @@ app.get("/todos/:id/edit", function(req, res){
 });
 
 app.put("/todos/:id", function(req, res){
- Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, todo){
+ Todo.findByIdAndUpdate(req.params.id, req.body.todo, {new: true}, function(err, todo){
    if(err){
      console.log(err);
    } else {
-     if(req.xhr) {
-       res.json(todo)
-     } else {
-       res.redirect('/'); 
-     }
+      res.json(todo);
    }
  });
 });
@@ -82,11 +78,7 @@ app.delete("/todos/:id", function(req, res){
    if(err){
      console.log(err);
    } else {
-     if(req.xhr) {
-       res.json(todo);
-     } else {
-       res.redirect("/todos");
-     }
+     res.json(todo);
    }
  }); 
 });
